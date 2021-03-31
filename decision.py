26080e280f9343d6d7c30a97db3281fba239cdd9
WilsonFTEC <ftecwilson@gmail.com>
2021-03-31 22:46:54 +0800

    text changed

--- decision.py
+++ decision.py
@@ -1,22 +1,10 @@
-from sklearn import tree
 import tensorflow as tf
-features = [[140, 1], [130, 1], [150, 0], [170, 0], [110, 1], [180, 0]] # let 1 = smooth & 0 = bumpy
-fruits = [1, 1, 0, 0, 1, 0] # let 1 = apples & 0 = oranges
 
-
-#saved_model.summary()
-
-input = [[10,10,10,10,10,10,10,10,10,10]]
-#print(saved_model.predict(input))
+#input = [[10,10,10,10,10,10,10,10,10,10]]
 
 class Prediction:
     def __init__(self, data):
-        #self.clf = tree.DecisionTreeClassifier()
         self.dt = data
-        #self.txt = 1 if texture == 'smooth' else 0
-
-    # def train(self):
-    #     self.clf = self.clf.fit(features, fruits)
 
     def test(self):
         saved_model = tf.keras.models.load_model("saved_model/ins_model1")
